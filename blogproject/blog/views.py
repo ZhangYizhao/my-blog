e06898944d04bfac264d10a7e9b3479785406112
张亦钊 <156525798122163.com>
2019-04-22 14:11:53 +0800

    blogproject

---
+++ blogproject/blog/views.py
@@ -0,0 +1,73 @@
+import markdown
+
+from django.shortcuts import render, get_object_or_404
+from comments.forms import CommentForm
+from .models import Post, Category
+
+"""
+请使用下方的模板引擎方式。
+def index(request):
+    return HttpResponse("欢迎访问我的博客首页！")
+"""
+
+"""
+请使用下方真正的首页视图函数
+def index(request):
+    return render(request, 'blog/index.html', context={
+        'title': '我的博客首页',
+        'welcome': '欢迎访问我的博客首页'
+    })
+"""
+
+
+def index(request):
+    post_list = Post.objects.all().order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+"""
+请使用下方包含评论列表和评论表单的详情页视图
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.body = markdown.markdown(post.body,
+                                  extensions=[
+                                      'markdown.extensions.extra',
+                                      'markdown.extensions.codehilite',
+                                      'markdown.extensions.toc',
+                                  ])
+    return render(request, 'blog/detail.html', context={'post': post})
+"""
+
+
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.body = markdown.markdown(post.body,
+                                  extensions=[
+                                      'markdown.extensions.extra',
+                                      'markdown.extensions.codehilite',
+                                      'markdown.extensions.toc',
+                                  ])
+    # 记得在顶部导入 CommentForm
+    form = CommentForm()
+    # 获取这篇 post 下的全部评论
+    comment_list = post.comment_set.all()
+
+    # 将文章、表单、以及文章下的评论列表作为模板变量传给 detail.html 模板，以便渲染相应数据。
+    context = {'post': post,
+               'form': form,
+               'comment_list': comment_list
+               }
+    return render(request, 'blog/detail.html', context=context)
+
+
+def archives(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year,
+                                    created_time__month=month
+                                    ).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+def category(request, pk):
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
